1c499771619ba37b2d3c7826ff8209139dc08daf
Yasmika Saubhagya <it18209280@my.sliit.lk>
2021-10-08 21:24:00 -0700

    Chnage the medium service url to https

--- src/services/article.service.js
+++ src/services/article.service.js
@@ -1,7 +1,7 @@
 export const fetchArticlesApi = async () => {
   const response = await (
     await fetch(
-      "http://api.rss2json.com/v1/api.json?rss_url=http://medium.com/feed/@yasmikash"
+      "https://api.rss2json.com/v1/api.json?rss_url=http://medium.com/feed/@yasmikash"
     )
   ).json();
   return response;
